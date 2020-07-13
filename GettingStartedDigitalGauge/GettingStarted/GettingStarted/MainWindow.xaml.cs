d37d4be46a0ef62cf48383050a2712f9231f49f0
vigneshramesh <vignesh.ramesh@syncfusion.com>
2020-07-13 22:06:42 +0530

    DigitalGauge GettingStarted sample added.

---
+++ GettingStartedDigitalGauge/GettingStarted/GettingStarted/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Syncfusion.UI.Xaml.Gauges;
+using System.Windows;
+using System.Windows.Media;
+
+namespace GettingStarted
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //SfDigitalGauge digitalgauge = new SfDigitalGauge();
+            //digitalgauge.Height = 100;
+            //digitalgauge.Width = 375;
+            //digitalgauge.Value = "11:59:50 PM";
+            //digitalgauge.CharacterHeight = 60;
+            //digitalgauge.CharacterWidth = 25;
+            //digitalgauge.HorizontalAlignment = HorizontalAlignment.Center;
+            //digitalgauge.VerticalAlignment = VerticalAlignment.Center;
+            //digitalgauge.CharacterType = CharacterType.EightCrossEightDotMatrix;
+            //digitalgauge.CharacterStroke = (SolidColorBrush)new BrushConverter().ConvertFrom("#146CED");
+            //digitalgauge.DimmedBrush = (SolidColorBrush)new BrushConverter().ConvertFrom("#F2F2F2");
+            //this.Content = digitalgauge;
+        }
+    }
+}
